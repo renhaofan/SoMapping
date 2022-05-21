fadef4465f8b99c840b8f104d8cb72fb7ba31d97
renhaofan <yqykrhf@163.com>
2022-05-21 11:38:10 +0800

    delete this->UI_engine_ptr

--- plane_fusion_final/Main_engine/Main_engine.h
+++ plane_fusion_final/Main_engine/Main_engine.h
@@ -1,66 +1,66 @@
-#pragma once
+/**
+ *  Copyright (C) All rights reserved.
+ *  @file Main_engine.h
+ *  @brief class Main_engine header, Singleton object contains
+ *         environment_initializer, data_engine, SLAM_system, render_engine.
+ *  @author haofan ren, yqykrhf@163.com
+ *  @version beta 0.0
+ *  @date 22-5-21
+ */
 
-// Head file of components
+#pragma once
 #include "Data_engine/Data_engine.h"
 #include "Environment_Initializer/Environment_Initializer.h"
 #include "SLAM_system/SLAM_system.h"
 #include "UI_engine/UI_engine.h"
 
-//!
-/*!
-        \note	Singleton object!
-*/
+/**
+ * @brief Singleton object
+ */
 class Main_engine {
  public:
-  //! The pointer to this static object.
+  /** @brief The pointer to this static object. */
   static Main_engine *instance_ptr;
-  //! Member function for instantiating this static object.
+  /** @brief Member function for instantiating this static object. */
   static Main_engine *instance(void) {
     if (instance_ptr == nullptr) instance_ptr = new Main_engine();
+    //! check whether allocte memory succesfully.
+    if (instance_ptr == nullptr) {
+      fprintf(stderr,
+              "File %s, Line %d, Function %s(): "
+              "Failed to allocate main engine memory.\n",
+              __FILE__, __LINE__, __FUNCTION__);
+      throw "Failed to allocate main engine memory!";
+    }
     return instance_ptr;
   }
 
-  //! Environment initializer
+  /** @brief Environment initializer. */
   Environment_Initializer *environment_initializer;
-  //! Data loader/writer
+  /** @brief Data loader/writer. */
   Data_engine *data_engine;
-  //! SLAM system
+  /** @brief SLAM system. */
   SLAM_system *SLAM_system_ptr;
-  //! OpenGL user interface
-  UI_engine *UI_engine_ptr;
-  //
+  /** @brief Render engine. */
   Render_engine *render_engine_ptr;
 
-  //! Default constructor/destructor
+  /** @brief Default constructor. */
   Main_engine();
+  /** @brief Default deconstructor. */
   ~Main_engine();
 
-  //! Initiate all components
-  /*!
-          \param	argc					Input the frist
-     argument of main function.
-
-          \param	argv					Input the second
-     argument of main function.
-
-          \param	image_loader_ptr		The pointer of
-     Image_loader.
-
-          \param	output_folder			Output folder.(output
-     estimated trajectory, mesh, etc.)
-
-          \param	_is_ICL_NUIM_dataset	Dataset coordinate mode.
-
-          \return	void
-  */
+  /**
+   * @brief Initiate all components.
+   * @param argc Input the first argument of main() function.
+   * @param argv Input the second argument of main() function.
+   * @param image_loader_ptr The pointer of Image_loader.
+   * @param output_folder Output folder.(output estimated trajectory, mesh, etc.)
+   * @param _is_ICL_NUIM_dataset Dataset coordinate mode.
+   */
   void init(int argc, char **argv, Image_loader *image_loader_ptr,
             string output_folder = "./", bool _is_ICL_NUIM_dataset = false);
-
-  //! Run main loop
-  /*!
-          \return	void
-
-          \note	Run the main loop.
-  */
+  /**
+   * @brief Alias UI_engine::instance()->run().
+   */
   void run();
 };
