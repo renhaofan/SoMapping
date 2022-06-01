42792b277861e4b03e5e1c75aa7a9aa323b5969a
renhaofan <yqykrhf@163.com>
2022-06-01 11:05:09 +0800

    comment

--- plane_fusion_final/SLAM_system/SLAM_system_settings.cpp
+++ plane_fusion_final/SLAM_system/SLAM_system_settings.cpp
@@ -2,29 +2,29 @@
 
 #include "OurLib/my_math_functions.h"
 
-//
 SLAM_system_settings *SLAM_system_settings::instance_ptr = nullptr;
 
-//
 SLAM_system_settings::SLAM_system_settings() { this->set_to_default(); }
 SLAM_system_settings::~SLAM_system_settings() {}
 
-//
 void SLAM_system_settings::set_to_default() {
-  //
   this->generate_mesh_for_visualization = true;
-  //
   this->enable_plane_module = true;
 
-  //
   this->image_alginment_patch_width = 16;
   this->raycast_range_patch_width = 16;
-  //
+
   this->aligned_depth_size.width = 0;
   this->aligned_depth_size.height = 0;
   this->aligned_color_size.width = 0;
   this->aligned_color_size.height = 0;
-  //
+
+  /*
+  Note that the depth images provided in our dataset are already pre-registered
+  to the RGB images. Therefore, rectifying the depth images based on the
+  intrinsic parameters is not straight forward.
+  https://vision.in.tum.de/data/datasets/rgbd-dataset/file_formats#intrinsic_camera_calibration_of_the_kinect
+  */
   int calib_mode = 1;
   if (calib_mode == 0) {
     // ICL
@@ -33,69 +33,67 @@ void SLAM_system_settings::set_to_default() {
     this->sensor_params.sensor_cx = 320.0f;
     this->sensor_params.sensor_cy = 240.0f;
   } else if (calib_mode == 1) {
-    // Fr1
+    // TUM Fr1
     this->sensor_params.sensor_fx = 591.1;
     this->sensor_params.sensor_fy = 590.1;
     this->sensor_params.sensor_cx = 331.0;
     this->sensor_params.sensor_cy = 234.0;
   } else if (calib_mode == 2) {
-    // Fr2
+    // TUM Fr2
     this->sensor_params.sensor_fx = 580.8;
     this->sensor_params.sensor_fy = 581.8;
     this->sensor_params.sensor_cx = 308.8;
     this->sensor_params.sensor_cy = 253.0;
   } else if (calib_mode == 3) {
-    // Fr3
+    // TUM Fr3
     this->sensor_params.sensor_fx = 567.6;
     this->sensor_params.sensor_fy = 570.2;
     this->sensor_params.sensor_cx = 324.7;
     this->sensor_params.sensor_cy = 250.1;
   }
 
+  // TUM 16-bit PNG files
+  // reference
+  // https://vision.in.tum.de/data/datasets/rgbd-dataset/file_formats#intrinsic_camera_calibration_of_the_kinect
   this->sensor_params.sensor_scale = 5000.0f;
+
+
   // this->sensor_params.sensor_scale = 1000.0f;
 
   this->sensor_params.sensor_noise_ratio = 0.01f;
   this->sensor_params.min_range = 0.10f;
   this->sensor_params.max_range = 8.0f;
 
-  //
   this->number_of_hierarchy_layers = 3;
-  //
+
   for (int i = 0; i < MAX_LAYER_NUMBER; i++) this->max_iterate_times[i] = 20;
-  //
+
   this->convergence_threshold = 1e-4f;
   this->failure_threshold = 5e-1f;
 
-  //
   this->presegment_cell_width = 16;
   this->pixel_data_weight = 0.1f;
   this->normal_position_weight = 0.8f;
 }
 
-//
 void SLAM_system_settings::set_depth_image_size(const int &width,
                                                 const int &height,
                                                 bool align_flag) {
-  //
   this->aligned_depth_size.width =
       ceil_by_stride(width, this->image_alginment_patch_width);
   this->aligned_depth_size.height =
       ceil_by_stride(height, this->image_alginment_patch_width);
 }
 
-//
 void SLAM_system_settings::set_color_image_size(const int &width,
                                                 const int &height,
                                                 bool align_flag) {
-  //
   this->aligned_color_size.width =
       ceil_by_stride(width, this->image_alginment_patch_width);
   this->aligned_color_size.height =
       ceil_by_stride(height, this->image_alginment_patch_width);
 }
 
-//
 void SLAM_system_settings::set_calibration_paramters(const float &fx,
                                                      const float &fy,
                                                      const float &cx,
@@ -115,6 +113,7 @@ void SLAM_system_settings::set_intrinsic(const float &fx, const float &fy,
   this->color_params.sensor_cx = cx;
   this->color_params.sensor_cy = cy;
 }
+
 void SLAM_system_settings::set_intrinsic(const float &fx, const float &fy,
                                          const float &cx, const float &cy,
                                          const float &scale) {
--- plane_fusion_final/SLAM_system/SLAM_system_settings.h
+++ plane_fusion_final/SLAM_system/SLAM_system_settings.h
@@ -1,5 +1,14 @@
-
-
+/**
+ *  Copyright (C) All rights reserved.
+ *  @file SLAM_system_settings.h
+ *  @brief Sensor info, tracker info, plane parameters and whether enable plane,
+ *         whether generate mesh for vis.
+ *  @author haofan ren, yqykrhf@163.com
+ *  @version beta 0.0
+ *  @date 22-5-21
+ *  @note Macro for different os
+ *        https://iq.opengenus.org/detect-operating-system-in-c/
+ */
 
 #pragma once
 
@@ -8,106 +17,180 @@
 #include "Config.h"
 
 //#define CUDA_CKECK_KERNEL
+/** @brief Check CUDA error. */
 #define CUDA_CKECK_KERNEL checkCudaErrors(cuCtxSynchronize());
 
+/**
+ * @brief Sensor parameters, such as fx,fy,cx,cy,depth range, scale, etc.
+ */
 typedef struct _Sensor_params {
-  //! Fx, Fy
-  float sensor_fx, sensor_fy;
-  //! Cx, Cy
-  float sensor_cx, sensor_cy;
-  //! Sensor scale (For example: Kinect scale = 1.0m / 1.0mm = 1000.0f)
+  /** @brief fx parameter. */
+  float sensor_fx;
+  /** @brief fy parameter. */
+  float sensor_fy;
+  /** @brief cx parameter. */
+  float sensor_cx;
+  /** @brief cy parameter. */
+  float sensor_cy;
+  /** @brief Sensor scale (For example: Kinect scale = 1.0m / 1.0mm = 1000.0f).
+   */
   float sensor_scale;
-  //! Sensor noise radius per meter
+  /** @brief Sensor noise radius per meter. */
   float sensor_noise_ratio;
-  //! Sensor range
-  float min_range, max_range;
+  /** @brief Sensor min range. */
+  float min_range;
+  /** @brief Sensor max range. */
+  float max_range;
 } Sensor_params;
 
-//!
-/*!
-
-*/
 class SLAM_system_settings {
  public:
-  //! The pointer to this static object.
+  /** @brief The pointer to this static object. */
   static SLAM_system_settings *instance_ptr;
-  //! Member function for instantiating this static object.
+  /** @brief Generate a instance for this static object. */
   static SLAM_system_settings *instance(void) {
     if (instance_ptr == nullptr) instance_ptr = new SLAM_system_settings();
     return instance_ptr;
   }
 
-  //! Generate mesh for better visualization
+  /** @brief Flag whether generate mesh for visualization. */
   bool generate_mesh_for_visualization;
-  //! Use plane information
+  /** @brief Flag whether use plane prior info. */
   bool enable_plane_module;
-
-#pragma region(Image / sensor parameters)
-  //! CUDA memory alignment size (must be setted to {1, 2, 4, ..., 16})
+#if __unix__
+#pragma region "Image / Sensor parameters" {
+#elif _WIN32
+#pragma region(Image / Sensor parameters)
+#endif
+  /**
+   *  @brief CUDA memory alignment size.
+   *  @warning Must be set in range of {1, 2, 4, ..., 16}.
+   */
   int image_alginment_patch_width;
-  //! Raycast range patch size (no bigger than
-  //! this->image_alginment_patch_width!)
+  /**
+   *  @brief Raycast range patch size.
+   *  @warning No bigger than this->image_alginment_patch_width!
+   */
   int raycast_range_patch_width;
 
-  //! Aligned depth image size
+  /** @brief Aligned depth image size. */
   My_Type::Vector2i aligned_depth_size;
-  //! Aligned color image size
+  /** @brief Aligned color image size. */
   My_Type::Vector2i aligned_color_size;
 
-  //! Sensor parameters
+  /** @brief Sensor parameters. */
   Sensor_params sensor_params;
+  /** @brief Color sensor parameters. */
   Sensor_params color_params;
+  /** @brief Depth sensor parameters. */
   Sensor_params depth_params;
-  My_Type::Matrix44f depth2color_mat, depth2imu_mat;
+  /** @brief Extrinsics from depth sensor to color sensor. */
+  My_Type::Matrix44f depth2color_mat;
+  /** @brief Extrinsics from depth sensor to imu sensor. */
+  My_Type::Matrix44f depth2imu_mat;
+#if _WIN32
 #pragma endregion
+#elif __unix__
+#pragma endregion }
+#endif
 
+#if __unix__
+#pragma region "Plane detector parameters" {
+#elif _WIN32
 #pragma region(Plane detector parameters)
-  //! Pre-segment cell width (NOTE: Must be the common divisor of
-  //! aligned_depth_size!)
+#endif
+  /**
+   * @brief Pre-segment cell width.
+   * @warning Must be the common divisor of aligned_depth_size.
+   */
   int presegment_cell_width;
-
-  //! Weight coefficients of super pixel cluster
+  /** @brief Weight coefficients of super pixel cluster. */
   float pixel_data_weight;
+  /** @brief Weight coefficients of super pixel cluster. */
   float normal_position_weight;
-
+#if _WIN32
 #pragma endregion
+#elif __unix__
+#pragma endregion }
+#endif
 
+#if __unix__
+#pragma region "Tracker parameters" {
+#elif _WIN32
 #pragma region(Tracker parameters)
-  //!
+#endif
+  /** @brief The number of hierarchy layers. */
   int number_of_hierarchy_layers;
-
-  //! Max iterate times of each layer
+  /** @brief Max iterate times of each layer. */
   int max_iterate_times[MAX_LAYER_NUMBER];
-
-  //! Convergence threshold of nonlinear solver (Tracker)
+  /** @brief Convergence threshold of nonlinear solver (Tracker). */
   float convergence_threshold;
-  //! Failed threshold of nonlinear solver (Tracker)
+  /** @brief Failed threshold of nonlinear solver (Tracker). */
   float failure_threshold;
-
+#if _WIN32
 #pragma endregion
+#elif __unix__
+#pragma endregion }
+#endif
 
-  //!
+  /** @brief Default constructor. Call this->set_to_default() */
   SLAM_system_settings();
+  /** @brief Default dstructor. */
   ~SLAM_system_settings();
 
-  //! Set all paramenters to default value.
+  /** @brief Set default values for parameters. */
   void set_to_default();
+  /**
+   * @brief Set color camera intrinsic matrix.
+   * @param fx Camera fx.
+   * @param fy Camera fy.
+   * @param cx Camera cx.
+   * @param cy Camera cy.
+   */
   void set_intrinsic(const float &fx, const float &fy, const float &cx,
                      const float &cy);
-  //! Set depth image size
+  /**
+   * @brief Set depth image size.
+   * @param width Assigned depth image width.
+   * @param height Assigned depth image height.
+   * @param align_flag Flag whether alignment, true by default.
+   */
   void set_depth_image_size(const int &width, const int &height,
                             bool align_flag = true);
-  //! Set color image size
+  /**
+   * @brief Set color image size.
+   * @param width Assigned color image width.
+   * @param height Assigned color image height.
+   * @param align_flag Flag whether alignment, true by default.
+   */
   void set_color_image_size(const int &width, const int &height,
                             bool align_flag = true);
-
-  //! Set calibration paramters
+  /**
+   * @brief Set Calibration Paramters.
+   * @param fx Camera fx.
+   * @param fy Camera fy.
+   * @param cx Camera cx.
+   * @param cy Camera cy.
+   * @param scale Camera scale.
+   */
   void set_calibration_paramters(const float &fx, const float &fy,
                                  const float &cx, const float &cy,
                                  const float &scale);
+  /**
+   * @brief Set depth camera intrinsic matrix.
+   * @param fx Depth camera fx.
+   * @param fy Depth camera fy.
+   * @param cx Depth camera cx.
+   * @param cy Depth camera cy.
+   * @param scale Depth Camera scale.
+   */
   void set_intrinsic(const float &fx, const float &fy, const float &cx,
                      const float &cy, const float &scale);
-
+  /**
+   * @brief Set extrinsic matrix.
+   * @param d2c Extrinsic from depth sensor to color sensor.
+   * @param d2i Extrinsic from depth sensor to imu sensor.
+   */
   void set_extrinsic(const My_Type::Matrix44f d2c,
                      const My_Type::Matrix44f d2i);
 };
